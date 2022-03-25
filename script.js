925428737f09e836b8128a02c43d974fc20fb336
nazimudheen267 <nazimudheen267@gmail.com>
2022-03-25 21:22:57 +0530

    scroll button added

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log('hello world')
+const scrollTop = document.getElementById('js-scrollTop');
+const topView = document.getElementById('js-top');
+
+// scroll event for visible button
+window.addEventListener('scroll', () => {
+    if (window.scrollY >= 424) {
+        scrollTop.style.visibility = "visible";
+    } else {
+        scrollTop.style.visibility = "hidden";
+    }
+})
+
+// click event for scroll top button
+scrollTop.addEventListener('click', () => {
+    topView.scrollIntoView({ behavior: "smooth" });
+})
